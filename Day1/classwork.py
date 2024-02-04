ea1543c20e421fca8ddce2d824d9029a36d7c930
levanikochelidze <Levaniko.chelidze25@outlook.com>
2024-02-04 22:08:23 +0400

    first commit

---
+++ Day1/classwork.py
@@ -0,0 +1,10 @@
+name="levani "
+# name-ცვლადი
+# = არის ცვლადის მნიშვნელობის მიმნიჭებელი
+# "levani" - ცვლადის მნიშვნელობა
+surname= "chelidze "
+age= "10 "
+height= "154"
+
+
+print(name + " " + surname + " " + age + " " + height)
