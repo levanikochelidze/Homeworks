79e4d4dc7dcb007bbc30616ff8fcaec0937300bd
levanikochelidze <Levaniko.chelidze25@outlook.com>
2024-03-11 14:39:39 +0400

    commit

---
+++ day5/homework.2/homework.py
@@ -0,0 +1,14 @@
+#weathe is trash
+#georgia city is tbilisi
+#i in chess i rearrange queen
+#girls have big parentheses
+#i have variety toys
+#incorrect test
+#i loss my phone
+#select goa
+#war handle
+#i tell teacher somthing
+#question survey
+#ensure children
+#similiar phone
+#homework explicit
